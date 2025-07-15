bb67aca6f31bf17dd349a15b7f4e1e8b17eea3a5
JoaoAugusto1374 <tauajoaoaugustoce@gmail.com>
2025-07-15 14:28:29 -0300

    add: método de agendar serviços

--- projetos/agendamento_servicos/Usuario.java
+++ projetos/agendamento_servicos/Usuario.java
@@ -11,11 +11,17 @@ public class Usuario {
         telefone = t;
     }
 
-    public void agendarServicos() {
+    public void agendarServicos(Prestador prest, String dataHora) {
         System.out.println("- Agendamento confirmado -");
         System.out.println("Nome: " + nome);
         System.out.println("email: " + email);
         System.out.println("telefone: " + telefone);
-        System.out.println("");
+        System.out.println("Prestador escolhido: " + prest.nome);
+        System.out.println("Serviço é: " + prest.servico);
+        System.out.println("O valor é de: " + prest.valorHora);
+        System.out.println("A data e hora do serviço é: " + dataHora);
     }
+
+    
+
 }
